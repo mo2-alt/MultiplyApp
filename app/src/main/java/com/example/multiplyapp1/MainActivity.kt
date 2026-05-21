f221f117c6c9bd50ba4e739aaa98a14793839b9a
mo2-alt <mohamednur2006@gmail.com>
2026-05-21 09:46:40 +0200

    .

--- app/src/main/java/com/example/multiplyapp1/MainActivity.kt
+++ app/src/main/java/com/example/multiplyapp1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiplyapp1
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
    var num1 = findViewById<EditText>(R.id.editNum1)
    var num2 = findViewById<EditText>(R.id.editNum2)
    var btnCalc = findViewById<Button>(R.id.btnCalc)
-   var txtans = findViewById<EditText>(R.id.txtAns)
+   var txtans = findViewById<TextView>(R.id.txtAns)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
