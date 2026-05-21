7c34bf3cfeb4fd441c1e48ac0307e3383217700e
mo2-alt <mohamednur2006@gmail.com>
2026-05-21 09:43:10 +0200

    added a variable

--- app/src/main/java/com/example/multiplyapp1/MainActivity.kt
+++ app/src/main/java/com/example/multiplyapp1/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.multiplyapp1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
 class MainActivity : AppCompatActivity() {
+   var num1 = findViewById<EditText>(R.id.editNum1)
+   var num2 = findViewById<EditText>(R.id.editNum2)
+   var btnCalc = findViewById<Button>(R.id.btnCalc)
+   var ans = findViewById<EditText>(R.id.txtAns)
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
